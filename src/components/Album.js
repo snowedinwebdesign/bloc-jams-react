3f460bc01752b2ebf24506157d5421a1a9a786e6
Ashley Brown <snowedin.webdesign@gmail.com>
2018-03-27 13:17:26 -0600

    Checkpoint 7 Audio Playback: done

--- src/components/Album.js
+++ src/components/Album.js
@@ -9,9 +9,35 @@ class Album extends Component {
 			return album.slug === this.props.match.params.slug
 		});
 		this.state = {
-			album: album 
+			album: album,
+			currentSong: album.songs[0],
+			isPlaying: false
 		};
+		this.audioElement = document.createElement('audio');
+		this.audioElement.src = album.songs[0].audioSrc;
 	}
+
+		play() {
+			this.audioElement.play();
+			this.setState({ isPlaying: true });
+		}
+		pause() {
+			this.audioElement.pause();
+			this.setState( { isPlaying: false });
+		}
+		setSong(song) {
+			this.audioElement.src = song.audioSrc;
+			this.setState({ currentSong: song })
+		}
+		handleSongClick(song) {
+			const isSameSong = this.state.currentSong === song;
+			if (this.state.isPlaying && isSameSong) {
+				this.pause();
+			} else {
+				if (!isSameSong) { this.setSong(song); }
+				this.play();
+			}
+		}
 	render() {
 		return (
 			<section className="album">
@@ -32,13 +58,18 @@ class Album extends Component {
 			         <tbody>
 			         	{
 					this.state.album.songs.map( (song, index) =>
-						<tr>
-							<td className="song-number">{ index+1 }</td>
-							<td className="song-title"> { song.title }</td>
-							<td className='song-duration"'>{ song.duration }</td>
-						</tr>
-						)
-				}
+						<tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
+                 			<td className="song-actions">
+                   				<button>
+				                     <span className="song-number">{index+1}</span>
+				                     <span className="ion-play"></span>
+				                     <span className="ion-pause"></span>
+                   				</button>
+	                 			</td>
+	                 			<td className="song-title">{song.title}</td>
+	                 			<td className="song-duration">{song.duration}</td>
+               			</tr>
+				)}
 			         </tbody>
          		</table>
 
