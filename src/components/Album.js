edde86e5eb2666e90b9784e54c3e3d2f4396d972
Ashley Brown <snowedin.webdesign@gmail.com>
2018-03-28 14:45:18 -0600

    Assignment 7 Player Bar Buttons: done

--- src/components/Album.js
+++ src/components/Album.js
@@ -46,6 +46,14 @@ class Album extends Component {
 			this.setSong(newSong);
 			this.play(newSong);
 		}
+		handleNextClick() {
+			const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+			const newIndex = Math.max(0, currentIndex + 1);
+			const newSong = this.state.album.songs[newIndex];
+			this.setSong(newSong);
+			this.play(newSong);
+
+		}
 	render() {
 		return (
 			<section className="album">
@@ -85,6 +93,7 @@ class Album extends Component {
 					currentSong={this.state.currentSong} 
 					handleSongClick={() => this.handleSongClick(this.state.currentSong)}
 					handlePrevClick={() => this.handlePrevClick()}
+					handleNextClick={() => this.handleNextClick()}
 				/>
 			</section>
 			);
